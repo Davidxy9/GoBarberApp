46e732357d22b939d5fd9bc676eeefc00acbb308
Davidxy9 <davidwilliamlima@hotmail.com>
2020-11-09 10:56:37 -0300

    iniciação do back-end completa

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -7,29 +7,25 @@ const sessionsRouter = Router();
 // Rota: Receber a req, chamar outro arquivo, devolver uma resposta
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-    // tirar password da listagem
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
+  // tirar password da listagem
+  const userWithoutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
 
-    return response.json({ user: userWithoutPassword, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json({ user: userWithoutPassword, token });
 });
 
 export default sessionsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,37 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
 import './database/index';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    // eslint-disable-next-line no-console
+    console.error(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internacional server error',
+    });
+  },
+);
 
 /* eslint-disable no-console */
 app.listen(3333, () => {
